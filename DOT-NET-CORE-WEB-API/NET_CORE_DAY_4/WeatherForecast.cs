41645c190ce985e7fce42cec969d1430e524ed6b
fezzrootech <fezzey@roottechnologies.co.in>
2025-02-21 14:00:26 +0530

    Day-4 creation

---
+++ DOT-NET-CORE-WEB-API/NET_CORE_DAY_4/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace NET_CORE_DAY_4
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
