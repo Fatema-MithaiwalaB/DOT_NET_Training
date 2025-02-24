d39be655cb9f806446620f3129ae1b2aafbc58d0
fezzrootech <fezzey@roottechnologies.co.in>
2025-02-24 16:58:44 +0530

    DAY-5-JWT

---
+++ DOT-NET-CORE-WEB-API/NET_CORE_DAY_5/Controllers/JwtAuth.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security;
+using NET_CORE_DAY_5.Entities;
+using NET_CORE_DAY_5.Models;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace NET_CORE_DAY_5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtAuth(IConfiguration configuration) : ControllerBase
+    {
+        public static User user = new();
+        [HttpPost("Register")]
+        public ActionResult<User> Register (UserDTO request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDTO request)
+        {
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user,user.PasswordHash, request.Password) == PasswordVerificationResult.Failed) 
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
---
+++ DOT-NET-CORE-WEB-API/NET_CORE_DAY_5/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace NET_CORE_DAY_5.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+ 
+    }
+}
