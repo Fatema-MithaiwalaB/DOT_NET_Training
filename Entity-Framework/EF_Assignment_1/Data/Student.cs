8c1edfc07ffe43c32bbb2fcd12028bfdce45b5ff
fezzrootech <fezzey@roottechnologies.co.in>
2025-03-06 12:40:06 +0530

    Entity-Framework-Asg1

---
+++ Entity-Framework/EF_Assignment_1/Data/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EF_Assignment_1.Data
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+}
---
+++ Entity-Framework/EF_Assignment_1/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EF_Assignment_1.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Assignment_1.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
